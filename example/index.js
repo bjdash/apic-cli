45e9f8524bbcb2bf6f3ff65ef733d67f0c85af92
bjdash <bijaya.k.dash@gmail.com>
2020-04-05 21:03:35 +0100

    Run test Suites via web URL

--- example/index.js
+++ example/index.js
@@ -1,9 +1,9 @@
 const apicCli = require('../lib');
 
-// apicCli.run('.\\example\\ToDo demo.suit.apic', {
+// apicCli.run('.\\example\\ToDo demo-with-env.suit.apic', {
 //apicCli.run('.\\example\\x-form.suit.apic', {
 //apicCli.run('.\\example\\form-data.suit.apic', {
-apicCli.run('.\\example\\ToDo demo-with-env.suit.apic', {
+apicCli.run('http://localhost:8080/api/webAccess/APICSuite/123456abcdef-testsuite-demo?token=123456abcdef-testsuite-demo', {
     // environment: '.\\example\\APIC Todo demo-env.env.apic',
     reporters: 'cli,junit',
     responseData: true
